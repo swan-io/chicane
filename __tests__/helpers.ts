b886c1f1c3064d12273fa1720a467fa9895a2fbb
Mathieu Acthernoene <zoontek@gmail.com>
2021-11-26 17:29:50 +0100

    Make tests more atomic

--- __tests__/helpers.ts
+++ __tests__/helpers.ts
@@ -1,31 +1,41 @@
 import { first, isMultipleParam, isNonEmpty, isParam } from "../src/helpers";
 
-test("first", () => {
+test("first returns the first element of an array", () => {
   expect(first(["a", "b", "c"])).toBe("a");
   expect(first([1, 2, 3])).toBe(1);
+});
+
+test("first returns undefined if the array is empty", () => {
   expect(first([])).toBeUndefined();
 });
 
-test("isNonEmpty", () => {
+test("isNonEmpty returns true if the value is not an empty string", () => {
   expect(isNonEmpty("test")).toBe(true);
   expect(isNonEmpty(" ")).toBe(true);
+});
+
+test("isNonEmpty returns false if the value is an empty string", () => {
   expect(isNonEmpty("")).toBe(false);
 });
 
-test("isParam", () => {
+test("isParam returns true if the value is a well formatted route param", () => {
   expect(isParam(":test")).toBe(true);
   expect(isParam(":")).toBe(true);
   expect(isParam(": ")).toBe(true);
+});
 
+test("isParam returns false if the value is not a well formatted route param", () => {
   expect(isParam(" :test")).toBe(false);
   expect(isParam("test")).toBe(false);
 });
 
-test("isMultipleParam", () => {
+test("isMultipleParam returns true if the value is a well formatted route multiple param", () => {
   expect(isMultipleParam(":test[]")).toBe(true);
   expect(isMultipleParam(":[]")).toBe(true);
   expect(isMultipleParam(": []")).toBe(true);
+});
 
+test("isMultipleParam returns false if the value is not a well formatted route multiple param", () => {
   expect(isMultipleParam(" :test[]")).toBe(false);
   expect(isMultipleParam("test[]")).toBe(false);
   expect(isMultipleParam(":test]")).toBe(false);
