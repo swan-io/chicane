d6a3891c6c9eeb96dc0a861ae58fa3756dbb78f8
Mathieu Acthernoene <zoontek@gmail.com>
2022-05-16 11:31:34 +0200

    Add more complex tests

--- __tests__/concatRoutes.ts
+++ __tests__/concatRoutes.ts
@@ -23,4 +23,15 @@ test("concatRoutes perform proper routes concatenation", () => {
   expect(concatRouteStrings("/#:foo", "/")).toBe("/#:foo");
   expect(concatRouteStrings("/", "/#:bar")).toBe("/#:bar");
   expect(concatRouteStrings("/#:foo", "/#:bar")).toBe("/#:bar");
+
+  expect(concatRouteStrings("/foo", "/bar?:baz")).toBe("/foo/bar?:baz");
+  expect(concatRouteStrings("/foo#:bar", "/baz#:qux")).toBe("/foo/baz#:qux");
+
+  expect(
+    concatRouteStrings("/foo?:bar&:baz&:qux[]", "/quux?:corge&:grault[]"),
+  ).toBe("/foo/quux?:bar&:baz&:qux[]&:corge&:grault[]");
+
+  expect(
+    concatRouteStrings("/foo?:bar&:baz[]#:qux", "/quux?:corge#:grault"),
+  ).toBe("/foo/quux?:bar&:baz[]&:corge#:grault");
 });
