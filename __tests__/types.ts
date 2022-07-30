34ce7ad1571c50a238c2b9775bbc9aa6ca57fe5f
Mathieu Acthernoene <zoontek@gmail.com>
2022-07-30 12:03:31 +0200

    Add ConcatRoutes basic test

--- __tests__/types.ts
+++ __tests__/types.ts
@@ -1,13 +1,17 @@
 import { expectType } from "tsd";
 import { test } from "vitest";
-import { SplitAndFilterEmpty } from "../src/types";
+import { ConcatRoutes, SplitAndFilterEmpty } from "../src/types";
 
 // @ts-expect-error
 const identityType = <T>(): T => {};
 const compareTypes = <R, T extends R>() => expectType<R>(identityType<T>());
 
+test("ConcatRoutes", () => {
+  compareTypes<ConcatRoutes<"/foo", "/bar">, "/foo/bar">();
+});
+
 test("SplitAndFilterEmpty", () => {
-  compareTypes<["foo", "bar"], SplitAndFilterEmpty<"/foo/bar", "/">>();
-  compareTypes<["foo", "bar"], SplitAndFilterEmpty<"foo/bar/", "/">>();
-  compareTypes<["foo", "bar"], SplitAndFilterEmpty<"/foo/bar/", "/">>();
+  compareTypes<SplitAndFilterEmpty<"/foo/bar", "/">, ["foo", "bar"]>();
+  compareTypes<SplitAndFilterEmpty<"foo/bar/", "/">, ["foo", "bar"]>();
+  compareTypes<SplitAndFilterEmpty<"/foo/bar/", "/">, ["foo", "bar"]>();
 });
