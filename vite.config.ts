e8cab45bcb74b366a720a1d6f08ce945ac56aef4
Mathieu Acthernoene <zoontek@gmail.com>
2022-10-04 15:34:24 +0200

    Update vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "jsdom",
+    setupFiles: ["vitest-setup.ts"],
     exclude: ["__tests__/utils.ts"],
     include: ["__tests__/**/*.{ts,tsx}"],
   },
---
+++ vitest-setup.ts
@@ -0,0 +1,5 @@
+import { cleanup } from "@testing-library/react";
+import { afterEach } from "vitest";
+
+// https://testing-library.com/docs/react-testing-library/api/#cleanup
+afterEach(cleanup);
