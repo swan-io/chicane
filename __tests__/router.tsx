7da5b8b7cd52eaf2075a1fa0dac26c84af660de7
Mathieu Acthernoene <zoontek@gmail.com>
2023-05-24 18:27:09 +0200

    Add getRoute test

--- __tests__/router.tsx
+++ __tests__/router.tsx
@@ -15,7 +15,7 @@ const expectToHaveFocus = (element: Element) => {
 };
 
 describe("router", () => {
-  const { useRoute } = createRouter(routes);
+  const { getRoute, useRoute } = createRouter(routes);
 
   type RouteName = keyof typeof routes;
 
@@ -59,6 +59,44 @@ describe("router", () => {
       pushUnsafe("/unknown");
     });
 
+    expect(container.textContent).toContain("Not found");
+  });
+
+  test("getRoute: should match the correct route", () => {
+    const App = () => {
+      const route = getRoute(routesToMatch);
+
+      if (route === undefined) {
+        return <div> Not found </div>;
+      }
+
+      return (
+        <>
+          {route.name === "Home" ? (
+            <div> Home </div>
+          ) : route.name === "Profile" ? (
+            <div> Profile {route.params.username} </div>
+          ) : null}
+        </>
+      );
+    };
+
+    const { container, rerender } = render(<App />);
+
+    expect(container.textContent).toContain("Home");
+
+    act(() => {
+      pushUnsafe("/profile/zoontek");
+    });
+
+    rerender(<App />);
+    expect(container.textContent).toContain("Profile zoontek");
+
+    act(() => {
+      pushUnsafe("/unknown");
+    });
+
+    rerender(<App />);
     expect(container.textContent).toContain("Not found");
   });
 
