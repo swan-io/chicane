5b25d3685b15a55811e98723e6efa345aa93c323
Mathieu Acthernoene <zoontek@gmail.com>
2023-12-13 15:04:57 +0100

    Fix areRouteEqual

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,5 @@
+import { Params } from "./types";
+
 export const first = <T>(value: T[]): T | undefined => value[0];
 export const identity = <T>(value: T): T => value;
 export const isNonEmpty = (value: string): boolean => value !== "";
@@ -7,10 +9,30 @@ export const areParamsArrayEqual = (arrayA: string[], arrayB: string[]) =>
   arrayA.length === arrayB.length &&
   arrayA.every((a, index) => a === arrayB[index]);
 
+export const stableStringifyParams = (params: Params) => {
+  const keys = Object.keys(params).sort();
+  return JSON.stringify(keys.map((key) => [key, params[key]]));
+};
+
 export const areRouteEqual = (
-  routeA?: { key: string },
-  routeB?: { key: string },
-) => routeA?.key === routeB?.key;
+  routeA?: { name: string; params: Params },
+  routeB?: { name: string; params: Params },
+): boolean => {
+  const hasRouteA = typeof routeA !== "undefined";
+  const hasRouteB = typeof routeB !== "undefined";
+
+  if (!hasRouteA && !hasRouteB) {
+    return true;
+  }
+  if (!hasRouteA || !hasRouteB || routeA.name !== routeB.name) {
+    return false;
+  }
+
+  return (
+    stableStringifyParams(routeA.params) ===
+    stableStringifyParams(routeB.params)
+  );
+};
 
 export const isMultipleParam = (value: string): boolean =>
   value.startsWith(":") && value.endsWith("[]");
