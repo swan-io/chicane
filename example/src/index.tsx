ba90d9265ce85dd5da2584e41e8020f2bbc7bb8e
Mathieu Acthernoene <zoontek@gmail.com>
2023-12-13 14:48:04 +0100

    Remove example StrictMode

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,15 +1,8 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
 const container = document.getElementById("root");
 
 if (container != null) {
-  const root = createRoot(container);
-
-  root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
+  createRoot(container).render(<App />);
 }
