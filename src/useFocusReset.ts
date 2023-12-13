06d1b2d7c49e471775117b967f594931c8d94b17
Mathieu Acthernoene <zoontek@gmail.com>
2023-12-13 20:03:07 +0100

    Add comment

--- src/useFocusReset.ts
+++ src/useFocusReset.ts
@@ -9,6 +9,8 @@ const focusableElements: Record<string, true> = {
 };
 
 export const useFocusReset = (containerRef: React.RefObject<unknown>) => {
+  // Only refocus when the component has rerended with a new pathname
+  // We don't want to reset focus on search params / hash changes
   const focusKey = getLocation().raw.path;
 
   useEffect(() => {
@@ -16,22 +18,20 @@ export const useFocusReset = (containerRef: React.RefObject<unknown>) => {
 
     // Only focus after a history change for UX, so that areas outside routing
     // (e.g. navigation header) are available immediately to keyboard navigation
-    if (element == null || !hasInitialLocationChanged()) {
-      return;
+    if (element != null && hasInitialLocationChanged()) {
+      try {
+        // A tabIndex of -1 allows element to be programmatically focused but
+        // prevents keyboard focus, so we don't want to set the value on elements
+        // that support keyboard focus by default.
+        if (
+          element.getAttribute("tabIndex") == null &&
+          !focusableElements[element.nodeName]
+        ) {
+          element.setAttribute("tabIndex", "-1");
+        }
+
+        element.focus();
+      } catch {} // eslint-disable-line no-empty
     }
-
-    try {
-      // A tabIndex of -1 allows element to be programmatically focused but
-      // prevents keyboard focus, so we don't want to set the value on elements
-      // that support keyboard focus by default.
-      if (
-        element.getAttribute("tabIndex") == null &&
-        !focusableElements[element.nodeName]
-      ) {
-        element.setAttribute("tabIndex", "-1");
-      }
-
-      element.focus();
-    } catch {} // eslint-disable-line no-empty
   }, [containerRef, focusKey]);
 };
