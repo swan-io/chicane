182f0ede3036a6b71844c46217704a75baef5281
Mathieu Acthernoene <zoontek@gmail.com>
2025-11-25 18:24:57 +0100

    Rollback eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -41,6 +41,9 @@ export default [
       "no-param-reassign": "error",
       "object-shorthand": "error",
 
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+
       "@typescript-eslint/ban-ts-comment": [
         "error",
         { "ts-check": true, "ts-expect-error": false },
@@ -61,27 +64,6 @@ export default [
       "@typescript-eslint/prefer-nullish-coalescing": "error",
       "@typescript-eslint/prefer-optional-chain": "error",
       "@typescript-eslint/strict-boolean-expressions": "error",
-
-      // Core hooks rules
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-
-      // React Compiler rules
-      "react-hooks/config": "error",
-      "react-hooks/error-boundaries": "error",
-      "react-hooks/component-hook-factories": "error",
-      "react-hooks/gating": "error",
-      "react-hooks/globals": "error",
-      "react-hooks/immutability": "error",
-      "react-hooks/preserve-manual-memoization": "error",
-      "react-hooks/purity": "error",
-      "react-hooks/refs": "error",
-      "react-hooks/set-state-in-effect": "error",
-      "react-hooks/set-state-in-render": "error",
-      "react-hooks/static-components": "error",
-      "react-hooks/unsupported-syntax": "warn",
-      "react-hooks/use-memo": "error",
-      "react-hooks/incompatible-library": "warn",
     },
   },
 ];
